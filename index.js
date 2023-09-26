dcb9701ddc129927a26117291c2db06d9bf2ba1c
jsh <jeong9260@naver.com>
2023-09-27 02:11:59 +0900

    [ADD]실습 서버 + DB에서 글 목록 불러오기

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+const bodyParser = require('body-parser')
+const fs = require('fs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
+
+app.set('view engine', 'jade')
+
+app.locals.pretty = true
+
 const mysql = require('mysql2')
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -6,60 +21,62 @@ const connection = mysql.createConnection({
   database: 'sample',
 })
 connection.connect()
-/*
-//topic 테이블 조회
-connection.query('SELECT * FROM topic', function (err, rows, fields) {
-  //에러 발생 시 에러 출력
-  if (err) {
-    console.log(err)
-  } else {
-    for (var i = 0; i < rows.length; i++) {
-      console.log(rows[i].title)
-    }
-  }
+
+app.listen(8080, function () {
+  console.log('server on 8080')
 })
-connection.end()
-*/
-/*
-//테이블 정보 추가
-let params = ['node.js', 'node.js is ...', '2023-09-26', '2']
-connection.query(
-  'INSERT INTO topic (title, description, created,author_id) VALUES(?,?,?,?)',
-  params,
-  function (err, rows, fields) {
+app.get('/topic/form', function (req, res) {
+  fs.readdir('data', function (err, files) {
     if (err) {
       console.log(err)
+      res.status(500).send('Internal Server Error')
     } else {
-      console.log(rows.insertId)
+      res.render('form', { topics: files })
     }
-  }
-)
-connection.end()
-*/
-/*
-//테이블 정보 수정
-let params = ['Node.js', 'Node.js is ...', 8]
-connection.query(
-  'UPDATE topic SET title=?, description=? WHERE id=?',
-  params,
-  function (err, rows, fields) {
+  })
+})
+
+app.post('/form_receiver', function (req, res) {
+  const title = req.body.title
+  const des = req.body.des
+  fs.writeFile('data/' + title, des, function (err) {
     if (err) {
       console.log(err)
+      res.status(500).send('Internal Server Error')
     } else {
-      console.log(rows)
+      res.redirect('/topic/' + title)
     }
-  }
-)
-*/
-let params = [7]
-connection.query(
-  'DELETE FROM topic WHERE id = ?',
-  params,
-  function (err, rows, fields) {
+  })
+})
+
+app.get(['/topic', '/topic/:id'], function (req, res) {
+  let sql = 'SELECT id,title FROM topic'
+
+  connection.query(sql, function (err, rows, fields) {
+    res.render('view', { topics: rows })
+  })
+  /*
+  fs.readdir('data', function (err, files) {
     if (err) {
       console.log(err)
+      res.status(500).send('Internal Server Error')
     } else {
-      console.log(rows)
+      const id = req.params.id
+      if (id) {
+        // id 값이 존재
+        fs.readFile('data/' + id, 'utf8', function (err, data) {
+          if (err) {
+            console.log(err)
+            res.status(500).send('Internal Server Error')
+          } else {
+            res.render('view', { topics: files, title: id, des: data })
+          }
+        })
+      } else {
+        // id값이 없음
+        res.render('view', { topics: files, title: 'Hello', des: 'world' })
+      }
     }
-  }
-)
+  })
+  */
+})
