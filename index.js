ed98d4c418c6cb07bd851b28711e8b2edefe27a2
jsh <jeong9260@naver.com>
2023-09-27 03:41:58 +0900

    [ADD] 글 삭제 기능

--- index.js
+++ index.js
@@ -25,15 +25,14 @@ connection.connect()
 app.listen(8080, function () {
   console.log('server on 8080')
 })
+
+const date = new Date()
+
+const year = date.getFullYear()
+const month = ('0' + (date.getMonth() + 1)).slice(-2)
+const day = ('0' + date.getDate()).slice(-2)
+const dateStr = year + '-' + month + '-' + day
 app.get('/topic/form', function (req, res) {
-  // fs.readdir('data', function (err, files) {
-  //   if (err) {
-  //     console.log(err)
-  //     res.status(500).send('Internal Server Error')
-  //   } else {
-  //     res.render('form', { topics: files })
-  //   }
-  // })
   let sql = 'SELECT id,title FROM topic_backup'
   connection.query(sql, function (err, rows, fields) {
     if (err) {
@@ -48,7 +47,7 @@ app.post('/form_receiver', function (req, res) {
   const title = req.body.title
   const description = req.body.description
   const author = req.body.author
-  const created = req.body.created
+  const created = dateStr
   let sql =
     'INSERT INTO topic_backup (title,description,created,author) VALUES(?,?,?,?)'
 
@@ -64,14 +63,79 @@ app.post('/form_receiver', function (req, res) {
       }
     }
   )
-  // fs.writeFile('data/' + title, des, function (err) {
-  //   if (err) {
-  //     console.log(err)
-  //     res.status(500).send('Internal Server Error')
-  //   } else {
-  //     res.redirect('/topic/' + title)
-  //   }
-  // })
+})
+app.get('/topic/:id/edit', function (req, res) {
+  let sql = 'SELECT id,title FROM topic_backup'
+  connection.query(sql, function (err, rows, fileds) {
+    let id = req.params.id
+    if (id) {
+      let sql = 'SELECT * FROM topic_backup WHERE id=?'
+      connection.query(sql, [id], function (err, topic, fields) {
+        if (err) {
+          console.log(err)
+          res.status(500).send('Internal Server Error')
+        } else {
+          res.render('edit', { topics: rows, topic: topic[0] })
+        }
+      })
+    } else {
+      console.log('There is no id')
+      res.status(500).send('Internal SErvaer Error')
+    }
+  })
+})
+
+app.post('/topic/:id/edit', function (req, res) {
+  const title = req.body.title
+  const description = req.body.description
+  const author = req.body.author
+  const id = req.params.id
+  let sql = 'UPDATE topic_backup SET title=?, description=?,author=? WHERE id=?'
+  connection.query(
+    sql,
+    [title, description, author, id],
+    function (err, rows, fields) {
+      if (err) {
+        console.log(err)
+        res.status(500).send('Internal Server Error')
+      } else {
+        res.redirect('/topic/' + id)
+      }
+    }
+  )
+})
+app.get('/topic/:id/del', function (req, res) {
+  let sql = 'SELECT id,title FROM topic_backup'
+  let id = req.params.id
+  connection.query(sql, function (err, rows, fields) {
+    let sql = 'SELECT * FROM topic_backup WHERE id=?'
+    connection.query(sql, [id], function (err, topic) {
+      if (err) {
+        console.log(err)
+        res.status(500).send('Internal Server Error')
+      } else {
+        if (topic.length === 0) {
+          console.log('There is no id')
+          res.status(500).send('Internal Server Error')
+        } else {
+          res.render('del', { topics: rows, topic: topic[0] })
+        }
+      }
+    })
+  })
+})
+
+app.post('/topic/:id/del', function (req, res) {
+  let id = req.params.id
+  let sql = 'DELETE FROM topic_backup WHERE id=?'
+  connection.query(sql, [id], function (err, result) {
+    if (err) {
+      console.log(err)
+      res.status(500).send('Internal Server Error')
+    } else {
+      res.redirect('/topic')
+    }
+  })
 })
 
 app.get(['/topic', '/topic/:id'], function (req, res) {
@@ -93,28 +157,4 @@ app.get(['/topic', '/topic/:id'], function (req, res) {
       res.render('view', { topics: rows })
     }
   })
-  /*
-  fs.readdir('data', function (err, files) {
-    if (err) {
-      console.log(err)
-      res.status(500).send('Internal Server Error')
-    } else {
-      const id = req.params.id
-      if (id) {
-        // id 값이 존재
-        fs.readFile('data/' + id, 'utf8', function (err, data) {
-          if (err) {
-            console.log(err)
-            res.status(500).send('Internal Server Error')
-          } else {
-            res.render('view', { topics: files, title: id, des: data })
-          }
-        })
-      } else {
-        // id값이 없음
-        res.render('view', { topics: files, title: 'Hello', des: 'world' })
-      }
-    }
-  })
-  */
 })
